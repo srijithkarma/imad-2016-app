c56b6b76e0e2c248b1eeeb22777d785dd76c56b8
srijithkarma <nair.s@hotmail.com>
2016-09-28 05:47:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,53 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne - {
+    title: 'Article 1 | Nair Srijith',
+    heading: 'Article 1',
+    date: 'Sep 26, 2016',
+    content: `
+        <p>
+        This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. 
+        </p>
+        <p>
+            This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. 
+        </p>
+        <p>
+            This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. 
+        </p>`
+    
+};
+
+var htmlTemplate = `
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>    
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html>`;
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
