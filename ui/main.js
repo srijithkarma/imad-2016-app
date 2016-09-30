eb6da7f0de063e8cae1c841fd732785a021306eb
srijithkarma <nair.s@hotmail.com>
2016-09-30 11:47:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,4 @@ console.log('Loaded!');
 //Change the text of the main-text div
 var element = document.getElementById('main-text');
 
-element.innerHTML = "Content Ch due to execution of main.js"
+element.innerHTML = "Content Ch due to execution of main.js";
