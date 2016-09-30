b01c0d8585454460774e958f17e3329a321f729a
srijithkarma <nair.s@hotmail.com>
2016-09-30 11:47:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ function createTemplate (data) {
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/:articleName', function (req, res) {
